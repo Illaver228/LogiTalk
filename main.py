6400b641633230de8edc6c9e3145de8de59e5531
Illaver228 <150708193+Illaver228@users.noreply.github.com>
2025-09-13 13:21:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from customtkinter import*
-
+import socket
+import threading
 class Mainwindow(CTk):
     def __init__(self):
         super().__init__()
         self.geometry("400x300")
         self.title("LogicTalk")
 
-        self.frame = CTkFrame(self, width=200, height=self.winfo_height())
+
+        self.frame = CTkFrame(self, width=200, height=260, corner_radius=15)
         self.frame.pack_propagate(False)
-        self.frame.configure(width=0)
         self.frame.place(x=0, y=0)
         self.is_show_menu = False
-        self.width_frame = 0
 
         self.label = CTkLabel(self.frame, text="Ваше ім'я")
         self.label.pack(pady=40)
@@ -19,15 +19,16 @@ class Mainwindow(CTk):
         self.entry = CTkEntry(self.frame)
         self.entry.pack()
 
-        self.theme = CTkOptionMenu(self.frame, values=['dark', 'light'])
+        self.theme = CTkOptionMenu(self.frame, values=['dark', 'light'], command=self.change_theme)
         self.theme.pack(side="bottom", pady=20)
+
+        self.field = CTkTextbox(self, width=400, height=260, corner_radius=10)
+        self.field.place(x=0, y=0)
 
         self.button = CTkButton(self, text="→", width=30, command=self.show_menu)
         self.button.place(x=0, y=0)
 
 
-        self.field = CTkScrollableFrame(self, width=380)
-        self.field.place(x=0, y=30)
 
         self.messege = CTkEntry(self, placeholder_text="Введіть повідомлення", height=40, width=360)
         self.messege.place(x=0, y=260)
@@ -38,28 +39,40 @@ class Mainwindow(CTk):
     def show_menu(self):
         if self.is_show_menu:
             self.is_show_menu = False
-            self.frame.pack_propagate(False)
+            self.field.configure(width=0)
+            self.field.configure(widht=400)
+            self.field.place(x=0, y=0)
             self.button.configure(text="→")
         else:
             self.is_show_menu = True
-            self.frame.pack_propagate(True)
+            self.field.configure(width=200)
+            self.field.configure(width=200)
+            self.field.place(x=200, y=0)
             self.button.configure(text="←")
 
-    def adaptation_ui(self):
+
         
+    def change_theme(self, value):
+        if value == "dark":
+            set_appearance_mode = "dark"
+        else:
+            set_appearance_mode = "light"
+
+    def change_theme(self, value):
+        set_appearance_mode(value)
 
 
+    # додати повідомлення
+    def add_message(self, text):
+        self.field.configure(state='normal')
+        self.field.insetr(END, f"Я:" {text} \n)
+        self.field.configure(state="desadle")
+        
+    # надіслати повідомлення
+        
+    # отримати повідомлення
 
 
-
-
-
-
-    # def change_theme(self, value):
-    #     if value == "dark":
-    #         set_appearance_mode = "dark"
-    #     else:
-    #         set_appearance_mode = "light"
         
 
 
