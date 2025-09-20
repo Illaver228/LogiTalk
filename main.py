00a49e25e7333d4ed8f026dd3e3cf9f356845b28
Illaver228 <150708193+Illaver228@users.noreply.github.com>
2025-09-20 13:23:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from customtkinter import*
 import socket
 import threading
+
 class Mainwindow(CTk):
     def __init__(self):
         super().__init__()
@@ -33,14 +34,26 @@ class Mainwindow(CTk):
         self.messege = CTkEntry(self, placeholder_text="Введіть повідомлення", height=40, width=360)
         self.messege.place(x=0, y=260)
 
-        self.send = CTkButton(self, text="→", height=40, width=40)
+        self.send = CTkButton(self, text="→", height=40, width=40, command=self.add_message)
         self.send.place(x=360, y=260)
+        
+        # self.username = "Illa"
+        # try: 
+        #     self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #     self.client.connect(("0.0.0.0", 8080))
+        #     hello = f"{self.username} приєднався до чату! \n"
+        #     self.client.send(hello.encode('utf-8'))
+        #     threading.Thread(target=self.recv_message, daemon=True).start()
+        # except Exception as exp:
+        #     self.add_message(f"Не вдалося підключитися до сервера: "{exp})
+            
+
 
     def show_menu(self):
         if self.is_show_menu:
             self.is_show_menu = False
             self.field.configure(width=0)
-            self.field.configure(widht=400)
+            self.field.configure(width=400)
             self.field.place(x=0, y=0)
             self.button.configure(text="→")
         else:
@@ -50,30 +63,50 @@ class Mainwindow(CTk):
             self.field.place(x=200, y=0)
             self.button.configure(text="←")
 
-
         
-    def change_theme(self, value):
-        if value == "dark":
-            set_appearance_mode = "dark"
-        else:
-            set_appearance_mode = "light"
-
     def change_theme(self, value):
         set_appearance_mode(value)
 
 
     # додати повідомлення
-    def add_message(self, text):
+    def add_message(self):
         self.field.configure(state='normal')
-        self.field.insetr(END, f"Я:" {text} \n)
-        self.field.configure(state="desadle")
+        text = self.messege.get()
+        self.field.insert(END, f"Я:{text} \n")
+        self.messege.delete(0, END)
+        self.field.configure(state="disable")
         
     # надіслати повідомлення
+    # def send_message(self):
+    #     get_message = self.message.get()
+    #     if get_message:
+    #         self.message(f"{self.username}: {get_message}")
+    #         data = f"{self.username}: {get_message} \n"
+    #         try:
+    #             self.client.sendall(data.encode())
+    #         except:
+    #             pass
+    # self.message.delete(0, END)
+            
+
         
     # отримати повідомлення
-
-
+    # def recv_message(self):
+    #     buffer = ""
+    #     while True:
+    #     try:
+    #         word = self.client.recv(4096)
+    #         i not word:
+    #             break
+    #             buffer += word.decode()
+    #             while "\n" in buffer:
+    #                 line, buffer = buffer,split("\n", 1)
+    #     except:
+    #         break
+    # self.client.close()
         
+
+      
 
 
 window = Mainwindow()
